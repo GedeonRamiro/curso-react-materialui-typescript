5a977da75d9a35913129150629447a993ee80581
Gedeon Ramiro <gedeon_ramiro@hotmail.com>
2022-10-14 18:52:05 -0300

    feat: Adicionando menu lateral

---
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -0,0 +1,60 @@
+import { Home } from "@mui/icons-material";
+import {
+  Avatar,
+  Divider,
+  ListItemIcon,
+  Drawer,
+  List,
+  ListItemButton,
+  ListItemText,
+  useTheme,
+} from "@mui/material";
+import { Box } from "@mui/system";
+
+interface IPros {
+  children: React.ReactNode;
+}
+
+export const MenuLateral = ({ children }: IPros) => {
+  const theme = useTheme();
+
+  return (
+    <>
+      <Drawer variant="permanent">
+        <Box
+          width={theme.spacing(28)}
+          height="100%"
+          display="flex"
+          flexDirection="column"
+        >
+          <Box
+            width="100%"
+            height={theme.spacing(20)}
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Avatar
+              sx={{ height: theme.spacing(12), width: theme.spacing(12) }}
+              src="https://loremflickr.com/500/500"
+            />
+          </Box>
+          <Divider />
+          <Box flex={1}>
+            <List component="nav">
+              <ListItemButton>
+                <ListItemIcon>
+                  <Home />
+                </ListItemIcon>
+                <ListItemText primary="Página inicial" />
+              </ListItemButton>
+            </List>
+          </Box>
+        </Box>
+      </Drawer>
+      <Box height="100vh" marginLeft={theme.spacing(28)}>
+        {children}
+      </Box>
+    </>
+  );
+};
