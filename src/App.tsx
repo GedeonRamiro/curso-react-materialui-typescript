5bb61bbafe67a2c9028deaf31fc285c40ba19023
Gedeon Ramiro <gedeon_ramiro@hotmail.com>
2022-10-14 14:30:06 -0300

    refactor: Remove imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import { ThemeProvider } from "@mui/material";
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes";
 import { AppThemeProvider } from "./shared/contexts";
-import { DarkTheme } from "./shared/themes";
 
 export const App = () => {
   return (
