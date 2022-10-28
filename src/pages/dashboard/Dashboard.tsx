3e7d66ad424d8570ed6ba1c975fee18c30482e42
Gedeon Ramiro <gedeon_ramiro@hotmail.com>
2022-10-28 14:39:43 -0300

    feat: Adicionado componente de barra de ferramentas

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,11 +1,14 @@
 import { Box } from "@mui/material";
 import { LayoutBaseDePagina } from "../../shared/layouts";
+import { BarraDeFerramentas } from "../../shared/components";
 
 export const Dashboard = () => {
   return (
     <LayoutBaseDePagina
       titulo="Página inicial"
-      barraDeFerramentas={<>Barra de ferramentas</>}
+      barraDeFerramentas={
+        <BarraDeFerramentas mostrarInputBusca textoBotaoNovo="Nova" />
+      }
     >
       <Box>testeando</Box>
     </LayoutBaseDePagina>
