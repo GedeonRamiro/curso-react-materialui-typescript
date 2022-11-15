6fe91e4c58bb4af821de2a917538684a766a489a
Gedeon Ramiro <gedeon_ramiro@hotmail.com>
2022-11-15 16:13:56 -0300

    feat: Adicionado hook de useDebounce

--- src/pages/pessoas/ListagemDePessoas.tsx
+++ src/pages/pessoas/ListagemDePessoas.tsx
@@ -1,22 +1,28 @@
 import { useEffect, useMemo } from "react";
 import { useSearchParams } from "react-router-dom";
 import { FerramentasDaListagem } from "../../shared/components";
+import { useDebounce } from "../../shared/hooks";
+
 import { LayoutBaseDePagina } from "../../shared/layouts";
 import { PessoaService } from "../../shared/services/api/pessoas/PessoasService";
 
 export const ListagemDePessoas = () => {
   const [searchParams, setSearchParams] = useSearchParams();
 
+  const { debounce } = useDebounce();
+
   const busca = useMemo(() => {
     return searchParams.get("busca") || "";
   }, [searchParams]);
 
   useEffect(() => {
-    PessoaService.getAll(1, busca).then((result) => {
-      if (result instanceof Error) {
-        return alert(result.message);
-      }
-      console.log(result);
+    debounce(() => {
+      PessoaService.getAll(1, busca).then((result) => {
+        if (result instanceof Error) {
+          return alert(result.message);
+        }
+        console.log(result);
+      });
     });
   }, [busca]);
 
