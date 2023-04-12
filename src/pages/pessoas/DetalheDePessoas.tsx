1396315b266069ec7cecaf9862413824e9cdaab1
Gedeon Ramiro <gedeon_ramiro@hotmail.com>
2023-04-12 15:40:34 -0300

    feat: Dados complexos e outras inputs com unform

--- src/pages/pessoas/DetalheDePessoas.tsx
+++ src/pages/pessoas/DetalheDePessoas.tsx
@@ -1,20 +1,29 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useNavigate, useParams } from "react-router";
 import { Form } from "@unform/web";
+import { FormHandles } from "@unform/core";
 import { FerramentasDeDetalhe } from "../../shared/components";
 import { LayoutBaseDePagina } from "../../shared/layouts";
 import { PessoaService } from "../../shared/services/api/pessoas/PessoasService";
 import { VTextField } from "../../shared/forms";
 
+interface IDetalhePessoa {
+  email: string;
+  cidadeId: number;
+  nomeCompleto: string;
+}
+
 export const DetalheDePessoas = () => {
   const { id = "nova" } = useParams<"id">();
   const navigate = useNavigate();
 
+  const formRef = useRef<FormHandles>(null);
+
   const [isLoading, setisLoading] = useState(false);
   const [nome, setNome] = useState("");
 
-  const handleSave = () => {
-    console.log("Save");
+  const handleSave = (dados: IDetalhePessoa) => {
+    console.log("dados:", dados);
   };
 
   const handleDelete = (id: number) => {
@@ -56,17 +65,18 @@ export const DetalheDePessoas = () => {
           mostrarBotaoSalvarEFechar
           mostrarBotaoApagar={id !== "nova"}
           mostrarBotaoNovo={id !== "nova"}
-          aoClicarEmSalvarEFechar={handleSave}
-          aoClicarEmSalvar={handleSave}
+          aoClicarEmSalvarEFechar={() => formRef.current?.submitForm()}
+          aoClicarEmSalvar={() => formRef.current?.submitForm()}
           aoClicarEmApagar={() => handleDelete(Number(id))}
           aoClicarEmVoltar={() => navigate("/pessoas")}
           aoClicarEmNovo={() => navigate("/pessoas/detalhe/nova")}
         ></FerramentasDeDetalhe>
       }
     >
-      <Form onSubmit={(dados) => console.log(dados)}>
+      <Form ref={formRef} onSubmit={handleSave}>
         <VTextField name="nomeCompleto" />
-        <button type="submit">enviar</button>
+        <VTextField name="email" />
+        <VTextField name="cidadeId" />
       </Form>
 
       <p>DetalheDePessoa</p>
